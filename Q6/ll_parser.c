f3610350d8df92ff63083840852a2ea4f8acbaee
George V Jose <jorujose@gmail.com>
2018-11-07 15:30:52 +0530

    LL(1) parser changes

--- Q6/ll_parser.c
+++ Q6/ll_parser.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
+#include<stdlib.h>
 
 int top = -1;
 int num_trows = 6, num_tcols =7;
+int ip_ptr = 0;
 char stack[25];
+char str[20];
+char cur_ip, cur_stack;
 char parsing_table[6][7][4] = { "NT","i","+","*","(",")","$",
 			                    "E" ,"TX","-","-","TX","-","-",
 		                    	"X","-","+TX","-","-","#","#",
@@ -15,17 +19,18 @@ char parsing_table[6][7][4] = { "NT","i","+","*","(",")","$",
 
 void push(char ch)
 {
+    printf("Pushing %c into stack.\n", ch);
     stack[++top] = ch;  
 }
 
-char pop(char ch)
+char pop()
 {
     return stack[top--];    
 }
 
 void revpush(char ch[])
 {
-    for(int i=strlen(ch); i>=0; i--)
+    for(int i=strlen(ch)-1; i>=0; i--)
         push(ch[i]);
 }
 
@@ -43,4 +48,68 @@ int getcol(char ch)
             return i;
 }
 
+void main()
+{
+    // Initialize stack
+    push('$');
+    push('E');
+
+    printf("Enter the input string: ");
+    scanf("%s", str);
+
+    while(top != -1)
+    {
+        cur_ip = str[ip_ptr];
+        cur_stack = pop();
+
+        // Check Terminal
+        if(cur_stack >= 'a' && cur_stack <= 'z' || 
+            cur_stack == '(' || cur_stack == ')' ||  
+            cur_stack == '+' || cur_stack == '-'  ||
+            cur_stack == '*' || cur_stack == '/')
+        {
+            printf("Terminal..");
+            if(cur_ip == cur_stack)
+            {
+                printf("%c matched.\n", cur_ip);
+                ip_ptr++;
+            }   
+            else
+            {
+                printf("\nInvalid Match. Stack: %c, Input: %c\n",cur_stack, cur_ip);
+                exit(0);
+            }
+        }
+
+        else
+        {
+            // row = current stack top, col = current input symbol
+            int row = getrow(cur_stack);
+            int col = getcol(cur_ip);
+
+            printf("Rule: %c --> %s", parsing_table[row][0][0], parsing_table[row][col]);
+
+            if(parsing_table[row][col][0] != '-')
+            {
+                if(strlen(parsing_table[row][col]) > 1)
+                    revpush(parsing_table[row][col]);
+                else if(parsing_table[row][col][0] != '#')
+                    push(parsing_table[row][col][0]);
+
+            }
+            else
+            {
+                printf("Parsing Table entry not found.\n");
+                exit(0);
+            }
+        }
+        if(str[ip_ptr] == '$')
+        {
+            printf("\n String Accepted.\n");
+            exit(0);
+        }
+
+    }
+
+}
 
